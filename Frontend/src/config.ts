db18a8bedf2bf8ead1655fc7b63c4e1f7ff91c55
Sohail khan <129187554+Sohailkhan2k02@users.noreply.github.com>
2024-06-23 22:30:44 +0530

    Delete Frontend/src/config.ts

--- Frontend/src/config.ts
+++
@@ -1,4 +0,0 @@
-export const BACKEND_URL = "https://backend.sksohail3390sk.workers.dev"
-
-// export const BACKEND_URL = "http://127.0.0.1:8787"
-export const AI_URL = "AIzaSyDAyiMoen1zf8vjyKYf7f0gxQRmzyvG3fs"
